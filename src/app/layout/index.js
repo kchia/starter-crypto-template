baa7fe5450944f9be53e9227da164f93e6623431
kchia <kchia87@gmail.com>
2021-09-16 15:04:33 -0400

    react-router setup: route to different pages

--- src/app/layout/index.js
+++ src/app/layout/index.js
@@ -1,3 +1,5 @@
+import { Route, Switch } from "react-router-dom";
+
 import {
   CoinsList,
   CoinView,
@@ -22,14 +24,35 @@ export default function Layout() {
         <Navigation />
       </div>
       <main className={mainContainer}>
-        <CoinsList />
-        <CoinView />
-        <CollectiblesList />
-        <CollectibleView />
-        <FavoritesList />
-        <FavoriteCreate />
-        <FavoriteEdit />
-        <NoMatch />
+        <Switch>
+          <Route exact path="/coins">
+            <CoinsList />
+          </Route>
+          <Route path="/coins/:id">
+            <CoinView />
+          </Route>
+
+          <Route exact path="/collectibles">
+            <CollectiblesList />
+          </Route>
+          <Route path="/collectibles/:address/:id">
+            <CollectibleView />
+          </Route>
+
+          <Route exact path="/favorites">
+            <FavoritesList />
+          </Route>
+          <Route path="/favorites/new">
+            <FavoriteCreate />
+          </Route>
+          <Route path="/favorites/:id/edit">
+            <FavoriteEdit />
+          </Route>
+
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
       </main>
       <Footer />
     </>
