e7e0494279fe4a493b53a06c275df6b8b6664183
kchia <kchia87@gmail.com>
2021-11-16 10:21:13 -0500

    mount router on /api path

--- src/features/projects/projects.service.js
+++ src/features/projects/projects.service.js
@@ -1,7 +1,7 @@
 import { client } from "../../api/client";
 import { formatNumber } from "../../common/utils";
 
-const API_URL = "/projects";
+const API_URL = "/api/projects";
 
 async function list(signal) {
   try {
