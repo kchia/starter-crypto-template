645aba9c763fcf2ac5a8f3a3c44fe1d2cff327de
kchia <kchia87@gmail.com>
2021-11-15 22:30:43 -0500

    remove /api from pathname

--- src/features/projects/projects.service.js
+++ src/features/projects/projects.service.js
@@ -1,7 +1,7 @@
 import { client } from "../../api/client";
 import { formatNumber } from "../../common/utils";
 
-const API_URL = "/api/projects";
+const API_URL = "/projects";
 
 async function list(signal) {
   try {
