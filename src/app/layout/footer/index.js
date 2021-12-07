747fc394473fe2b83fe8133d51341c25de3ea5ea
kchia <kchia87@gmail.com>
2021-12-06 22:05:45 -0500

    add linkedin info to footer

--- src/app/layout/footer/index.js
+++ src/app/layout/footer/index.js
@@ -2,7 +2,10 @@ import styles from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2021 by Hou Chia. All Rights Reserved.</p>
+      <p>
+        © 2021 by <a href="https://linkedin.com/in/houchia">Hou Chia</a>. All
+        Rights Reserved.
+      </p>
     </footer>
   );
 }
